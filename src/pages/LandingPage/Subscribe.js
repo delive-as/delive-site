9c5dd5ec485f566420fb0ae834e009c207757ec3
lockjio <jio.buenviaje@gmail.com>
2020-02-05 22:18:04 -0500

    Integrate Mailchimp API

--- src/pages/LandingPage/Subscribe.js
+++ src/pages/LandingPage/Subscribe.js
@@ -1,6 +1,12 @@
 // React Basic and Bootstrap
 import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
+import SubscribeForm from "./SubscribeForm";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import Particles from "react-particles-js";
+
+const url =
+	"https://DeLiveDrones.us4.list-manage.com/subscribe/post?u=348297cf2d50a2fd0e83fd12b&amp;id=ab27abf3a8";
 
 class Subscribe extends Component {
 	render() {
@@ -8,6 +14,82 @@ class Subscribe extends Component {
 			<React.Fragment>
 				<section className="section bg-light">
 					<div className="container">
+						<Particles
+							params={{
+								particles: {
+									number: {
+										value: 50,
+										density: {
+											enable: false
+										}
+									},
+									color: {
+										value: "#d81935"
+									},
+									shape: {
+										type: "polygon",
+										stroke: {
+											width: 2,
+											color: "#d81935"
+										},
+										polygon: {
+											nb_sides: 7
+										},
+										image: {
+											src: "img/github.svg",
+											width: 100,
+											height: 100
+										}
+									},
+									opacity: {
+										value: 1,
+										random: true,
+										anim: {
+											enable: true,
+											speed: 0.25,
+											opacity_min: 0,
+											sync: false
+										}
+									},
+									size: {
+										value: 1
+									},
+									line_linked: {
+										enable: false,
+										color: "#202943",
+										opacity: 1
+									},
+									move: {
+										enable: true,
+										speed: 5,
+										direction: "top-left",
+										random: false,
+										straight: true,
+										out_mode: "out"
+									}
+								},
+								interactivity: {
+									events: {
+										onhover: {
+											enable: true,
+											mode: "grab"
+										},
+										onclick: {
+											enable: true,
+											mode: "push"
+										}
+									}
+								}
+							}}
+							style={{
+								width: "100%",
+								height: "auto",
+								backgroundColor: "#f8f9fc",
+								position: "absolute",
+								top: "0",
+								left: "0"
+							}}
+						/>
 						<Row className="justify-content-center">
 							<Col className="text-center">
 								<div className="section-title mb-4 pb-2">
@@ -35,29 +117,22 @@ class Subscribe extends Component {
 
 						<Row className="justify-content-center mt-4 pt-2">
 							<Col lg={7} md={10}>
-								<form>
-									<div className="form-group">
-										<div className="input-group mb-3">
-											<input
-												name="email"
-												id="email"
-												type="email"
-												className="form-control"
-												placeholder="Your email"
-												required=""
-											/>
-											<div className="input-group-append">
-												<button
-													className="btn btn-primary submitBnt"
-													type="button"
-													id="newssubscribebtn"
-												>
-													Subscribe
-												</button>
-											</div>
-										</div>
-									</div>
-								</form>
+								<MailchimpSubscribe
+									url={url}
+									render={({
+										subscribe,
+										status,
+										message
+									}) => (
+										<SubscribeForm
+											status={status}
+											message={message}
+											onValidated={formData =>
+												subscribe(formData)
+											}
+										/>
+									)}
+								/>
 							</Col>
 						</Row>
 					</div>
---
+++ src/pages/LandingPage/SubscribeForm.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const SubscribeForm = ({ status, message, onValidated }) => {
+	let email;
+	const submit = () =>
+		email &&
+		email.value.indexOf("@") > -1 &&
+		onValidated({
+			EMAIL: email.value
+		});
+
+	return (
+		<form>
+			<div className="form-group">
+				<div className="input-group mb-3">
+					<input
+						ref={node => (email = node)}
+						type="email"
+						name="email"
+						id="email"
+						className="form-control"
+						placeholder="Your email"
+						required=""
+					/>
+					<div className="input-group-append">
+						<button
+							className="btn btn-primary submitBnt"
+							type="button"
+							id="newssubscribebtn"
+							onClick={submit}
+						>
+							Subscribe
+						</button>
+					</div>
+				</div>
+			</div>
+		</form>
+	);
+};
+
+export default SubscribeForm;
