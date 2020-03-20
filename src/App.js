789c5dd9f1534f6d9b905c172fec4d6b53c33a0a
lockjio <jio.buenviaje@gmail.com>
2020-03-20 04:25:44 -0400

    Add metadata for PWA audit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 	BrowserRouter as Router,
 	withRouter
 } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import Layout from "./components/Layout/";
 
@@ -17,9 +18,58 @@ function withLayout(WrappedComponent) {
 	return class extends React.Component {
 		render() {
 			return (
-				<Layout>
-					<WrappedComponent></WrappedComponent>
-				</Layout>
+				<>
+					<Helmet>
+						<meta charSet="utf-8" />
+						<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+						<meta
+							name="viewport"
+							content="width=device-width, initial-scale=1, shrink-to-fit=no"
+						/>
+						<meta name="author" content="DeLive Drones" />
+						<meta property="og:type" content="website" />
+						<meta property="og:site_name" content="DeLive Drones" />
+						<meta property="og:url" content="delivedrones.com" />
+						<meta
+							property="twitter:url"
+							content="delivedrones.com"
+						/>
+						<meta name="theme-color" content="#ffffff" />
+						<title>DeLive Drones</title>
+						<meta property="og:title" content="DeLive Drones" />
+						<meta
+							property="twitter:title"
+							content="DeLive Drones"
+						/>
+						<meta
+							name="description"
+							content="At DeLive, we aim to revolutionize emergency medical services (EMS) and save more lives by arriving earlier than the first responder with drone fly-ahead technology."
+						/>
+						<meta
+							property="og:description"
+							content="At DeLive, we aim to revolutionize emergency medical services (EMS) and save more lives by arriving earlier than the first responder with drone fly-ahead technology."
+						/>
+						<meta
+							property="twitter:description"
+							content="At DeLive, we aim to revolutionize emergency medical services (EMS) and save more lives by arriving earlier than the first responder with drone fly-ahead technology."
+						/>
+						<meta
+							name="keywords"
+							content="delive,drones,delivedrones,deliveaerial,deliveaerialsystems,aerialsystems,unmanneddrones,unmannedaerialdrones,uad,emergency,medical,services,emergencymedicalservices,medicalservices,ems,firstresponder,nd,notredame,esteem,esteemnd,southbend,indiana,university,flyahead,drone,dronesasaservice,daas,ideacenter,innovationpark,ces,consumerelectronicsshow,consumerelectronics,consumerelectronicsshow2020,ces2020,notre,dame,und,racetorevenue,universityofnotredame,ndideacenter,ndinnovationpark"
+						/>
+						<meta
+							name="twitter:card"
+							content="summary_large_image"
+						/>
+						<link
+							rel="canonical"
+							href="https://delivedrones.com/"
+						/>
+					</Helmet>
+					<Layout>
+						<WrappedComponent></WrappedComponent>
+					</Layout>
+				</>
 			);
 		}
 	};
